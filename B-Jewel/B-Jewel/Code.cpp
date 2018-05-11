728c1e9b68867cb202b276039c575263af9f8270
Fernando Gonzalez-Clara <gonzalez.ferny31@outlook.com>
2018-05-11 09:50:45 -0600

    5-11-18

--- B-Jewel/B-Jewel/Code.cpp
+++ B-Jewel/B-Jewel/Code.cpp
@@ -1,90 +1,102 @@
+/////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <ctime>
 #include <allegro5/allegro.h>
 #include<allegro5/allegro_primitives.h>
 #include<allegro5/allegro_image.h>
-void Clear(int Map[10][10]);
-
 using namespace std;
+/////////////////////////////////////////////////////////////////////////
+bool Clear(int Map[10][10]);
 const int Size = 40;
 const int Height = 500;
 const int Width = 500;
-const int FPS = 60;
+const int FPS = 1;
 int mouseX = Width / 2.0 - Size / 2.0;
 int mouseY = Height / 2.0 - Size / 2.0;
+int boxX;
+int boxY;
+int snapX = 0;
+int snapY = 0;
+void JemFall(int Map[10][10]);
+void Replenish(int Map[10][10]);
+int highlightX = 0;
+int highlightY = 0;
+bool highlight = false;
+int leftovers = 0;
+bool snapped = false;
 
-
+int highlight2X = 0;
+int highlight2Y = 0;
+bool highlight2 = false;
+int leftovers2 = 0;
+bool snapped2 = false;
+int temp;
+/////////////////////////////////////////////////////////////////////////
 int main() {
+/////////////////////////////////////////////////////////////////////////
 	al_init();
 	al_init_primitives_addon();
 	al_init_image_addon();
 	al_install_keyboard();
 	al_install_mouse();
 	srand(time(NULL));
-
+/////////////////////////////////////////////////////////////////////////
 	ALLEGRO_DISPLAY *Display = al_create_display(Height, Width);
 	ALLEGRO_EVENT_QUEUE *Event_Queue = al_create_event_queue();
 	ALLEGRO_TIMER *Timer = al_create_timer(1.0 / FPS);
-	ALLEGRO_BITMAP *Emerald = al_load_bitmap("Emerald.png"); //al_create_bitmap(Size, Size);
-	ALLEGRO_BITMAP *Diamond = al_load_bitmap("Diamond.png"); //al_create_bitmap(Size, Size);
-	ALLEGRO_BITMAP *Ruby = al_load_bitmap("Ruby.png"); //al_create_bitmap(Size, Size);
-	ALLEGRO_BITMAP *Sapphire = al_load_bitmap("Sapphire.png"); //al_create_bitmap(Size, Size);
+	ALLEGRO_BITMAP *Emerald = al_load_bitmap("Emerald.png");
+	ALLEGRO_BITMAP *Diamond = al_load_bitmap("Diamond.png");
+	ALLEGRO_BITMAP *Ruby = al_load_bitmap("Ruby.png");
+	ALLEGRO_BITMAP *Sapphire = al_load_bitmap("Sapphire.png"); 
 	ALLEGRO_BITMAP *MouseXY = al_create_bitmap(Size, Size);
-
+/////////////////////////////////////////////////////////////////////////
 	bool Redraw = true;
 	bool Doexit = false;
 	bool clicked = false;
-	int boxX;
-	int boxY;
-	int snapX = 0;
-	int snapY = 0;
-
+/////////////////////////////////////////////////////////////////////////
 	al_set_target_bitmap(Emerald);
-	//al_clear_to_color(al_map_rgb(21, 214, 72));
 	al_set_target_bitmap(Diamond);
-	//al_clear_to_color(al_map_rgb(214, 214, 20));
 	al_set_target_bitmap(Ruby);
-	//al_clear_to_color(al_map_rgb(214, 19, 19));
 	al_set_target_bitmap(Sapphire);
-	//al_clear_to_color(al_map_rgb(41, 18, 214));
 	al_set_target_bitmap(MouseXY);
 	al_clear_to_color(al_map_rgb(41, 18, 214));
 	al_set_target_bitmap(al_get_backbuffer(Display));
-
+/////////////////////////////////////////////////////////////////////////
 	al_register_event_source(Event_Queue, al_get_display_event_source(Display));
 	al_register_event_source(Event_Queue, al_get_timer_event_source(Timer));
 	al_register_event_source(Event_Queue, al_get_keyboard_event_source());
 	al_register_event_source(Event_Queue, al_get_mouse_event_source());
-
+/////////////////////////////////////////////////////////////////////////
 	al_clear_to_color(al_map_rgb(0, 0, 0));
-
 	al_flip_display();
-
 	al_start_timer(Timer);
-
+/////////////////////////////////////////////////////////////////////////
 	int Map[10][10];
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			Map[i][j] = rand() % 4 + 1;
+			
 		}
 	}
-
+	while (Clear(Map)) {
+		JemFall(Map);
+		Replenish(Map);
+	}
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++)
 			cout << Map[i][j];
 		cout << endl;
 	}
-
+/////////////////////////////////////////////////////////////////////////
 	al_convert_mask_to_alpha(Emerald, al_map_rgb(255, 255, 255));
-	al_convert_mask_to_alpha(Diamond, al_map_rgb(255, 255, 255));
+	al_convert_mask_to_alpha(Diamond, al_map_rgb(0, 0, 0));
 	al_convert_mask_to_alpha(Ruby, al_map_rgb(255, 255, 255));
 	al_convert_mask_to_alpha(Sapphire, al_map_rgb(255, 255, 255));
-
+/////////////////////////////////////////////////////////////////////////
+	
 	while (!Doexit) {
-		//Checks Game Board for any Allegro Variable that are three or more.
-		//in any given row or colloum. Then regenerates another Variable.
-		Clear(Map);
-		/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 		/*int Map[10][10];
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
@@ -96,36 +108,36 @@ int main() {
 			cout << Map[i][j];
 		cout << endl;
 		}*/
-		/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(Event_Queue, &ev);
-		/////////////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////////////	
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			if (!snapped) {
+				leftovers = highlightX % 50;
+				highlightX -= leftovers;
+				leftovers = highlightY % 50;
+				highlightY -= leftovers;
+				snapped = true;
+			}
+			if (!snapped2) {
+				leftovers2 = highlight2X % 50;
+				highlight2X -= leftovers2;
+				leftovers2 = highlight2Y % 50;
+				highlight2Y -= leftovers2;
+				snapped = true;
+			}
+
+
 			Redraw = true;
 		}
-		/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES ||
 			ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
-			mouseX = ev.mouse.x; //- 20;
-			mouseY = ev.mouse.y; //-20;
+			mouseX = ev.mouse.x;
+			mouseY = ev.mouse.y; 
 		}
-		/////////////////////////////////////////////////////////////////////////
-			//else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-			//	for (int i = 0; i < 10; i++) {
-			//		for (int j = 0; j < 10; j++) {
-			//			if (Map[mouseX / 50][mouseY / 50] == 1 ||
-			//				Map[mouseX / 50][mouseY / 50] == 2 || 
-			//				Map[mouseX / 50][mouseY / 50] == 3 ||
-			//				Map[mouseX / 50][mouseY / 50] == 4) {
-			//					al_draw_rectangle(i, j, i + 50, j + 50, al_map_rgb(50, 80, 90), 3);
-			//				//al_draw_rectangle(i,j, mouseX+50, mouseY+50, al_map_rgb(50,80,90), 3);
-			//				cout << "Clicked" << endl;
-			//				//al_draw_rectangle(i * 50, j * 50, i * 50 + 50, j * 50 + 50, al_map_rgb(50, 80, 90), 3);
-			//			}
-			//		}
-			//	}
-			//}
-		/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
 			case ALLEGRO_KEY_ESCAPE:
@@ -133,28 +145,42 @@ int main() {
 				break;
 			}
 		}
-
+/////////////////////////////////////////////////////////////////////////
 		if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			/*for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-			if (Map[mouseX / 50][mouseY / 50] == 1 || Map[mouseX / 50][mouseY / 50] == 2 ||
-			Map[mouseX / 50][mouseY / 50] == 3 || Map[mouseX / 50][mouseY / 50] == 4) {
-			*/
-			//al_draw_rectangle(mouseX, mouseY, mouseX + 50, mouseY + 50, al_map_rgb(255, 255, 0), 3);
-			clicked = true;
+		/*	clicked = true;
 			boxX = mouseX;
 			boxY = mouseY;
-
-
 			snapX = boxX % 50;
 			boxX -= snapX;
 			snapY = boxY % 50;
 			boxY -= snapY;
-
-
-			cout << "Clicked" << endl;
-			//al_draw_rectangle(i,j, mouseX+50, mouseY+50, al_map_rgb(50,80,90), 3);
-			//al_draw_rectangle(i * 50, j * 50, i * 50 + 50, j * 50 + 50, al_map_rgb(50, 80, 90), 3);
+			cout << boxX << ", " << boxY << endl;*/
+			if(highlight == true && ev.mouse.x > highlightX - 40 && ev.mouse.x < highlightX + 80 && ev.mouse.y > highlightY - 40 && ev.mouse.y < highlightY + 80) {
+				//cout << "selected next slot!";
+				highlight2 = true;
+				highlight2X = ev.mouse.x;
+				highlight2Y = ev.mouse.y;
+				snapped2 = false;
+				//swap the two boxes 
+				temp = Map[highlight2X / 50][highlight2Y / 50];
+				Map[highlight2X / 50][highlight2Y / 50] = Map[highlightX / 50][highlightY / 50];
+				Map[highlightX / 50][highlightY / 50] = temp;
+				while (Clear(Map)) {
+					JemFall(Map);
+					Replenish(Map);
+					highlight = false;
+					highlight2 = false;
+				}
+			}
+			//otherwise, select a new box
+		else
+		{
+			highlight = true;
+			highlight2 = false;
+			highlightX = ev.mouse.x;
+			highlightY = ev.mouse.y;
+			snapped = false;
+		}
 		}
 /////////////////////////////////////////////////////////////////////////
 		if (Redraw && al_is_event_queue_empty(Event_Queue)) {
@@ -162,7 +188,7 @@ int main() {
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 10; j++) {
-					//al_draw_rectangle(i*50,j*50, i*50+50, j*50+50, al_map_rgb(50,80,90), 3);
+				//	al_draw_rectangle(i*50,j*50, i*50+50, j*50+50, al_map_rgb(50,80,90), 3);
 					al_draw_rectangle(i * 50, j * 50, i * 50, j * 50, al_map_rgb(50, 80, 90), 5);
 					if (Map[j][i] == 1) {
 						al_draw_bitmap(Emerald, j * 50 + 5, i * 50 + 5, 0);
@@ -178,8 +204,14 @@ int main() {
 					}
 				}
 			}
-			if (clicked)
-				al_draw_rectangle(boxX, boxY, boxX + 50, boxY + 50, al_map_rgb(255, 255, 0), 3);
+			//if (clicked) { al_draw_rectangle(boxX, boxY, boxX + 50, boxY + 50, al_map_rgb(255, 255, 0), 3); }
+			if (highlight) {
+				al_draw_rectangle(highlightX, highlightY, highlightX + 50, highlightY + 50, al_map_rgb(255, 255, 255), 5);
+			}
+			if (highlight2) {
+				al_draw_rectangle(highlight2X, highlight2Y, highlight2X + 50, highlight2Y + 50, al_map_rgb(0, 255, 255), 5);
+			}
+			//JemFall(Map);
 			al_flip_display();
 		}//end render
 	}//end game loop
@@ -188,24 +220,80 @@ int main() {
 	al_destroy_bitmap(Diamond);
 	al_destroy_bitmap(Ruby);
 	al_destroy_bitmap(Sapphire);
+	al_destroy_bitmap(MouseXY);
 	al_destroy_timer(Timer);
 	al_destroy_display(Display);
 	al_destroy_event_queue(Event_Queue);
-	al_destroy_bitmap(MouseXY);
 /////////////////////////////////////////////////////////////////////////
 	return 0;
 }//Main Loop
  /////////////////////////////////////////////////////////////////////////
-void Clear(int Map[10][10]) {
+/*void Clear(int Map[10][10]) {
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			if (Map[i][j] == Map[i][j + 1] && Map[i][j] == Map[i][j + 2]) {
-				Map[i][j] = rand() % 4 + 1;
+				Map[i][j] = 0; //rand() % 4 + 1;
 			}
 			if (Map[i + 1][j] == Map[i][j] && Map[i + 2][j] == Map[i][j]) {
+				Map[i][j] = 0; //rand() % 4 + 1;
+			}
+		}
+	}
+}*/
+bool Clear(int Map[10][10]) {
+	int count = 1;
+	bool foundMatch = false;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (Map[i][j] == Map[i][j + 1] && Map[i][j] == Map[i][j + 2]) {
+				foundMatch = true;
+				while (Map[i][j] == Map[i][j + count]) {
+					Map[i][j + count] = 0;
+					count++;
+				}
+				Map[i][j] = 0;
+				count = 1;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if ((Map[i][j] == Map[i + 1][j]) && (Map[i][j] == Map[i + 2][j])) {
+				foundMatch = true;
+				while (Map[i][j] == Map[i + count][j]) {
+					Map[i + count][j] = 0;
+					count++;
+				}
+				Map[i][j] = 0;
+				count = 1;
+			}
+		}
+	}
+	return foundMatch;
+}
+/////////////////////////////////////////////////////////////////////////
+void JemFall(int Map[10][10]) {
+	int counter = 1;
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < 10; j++) {
+			if (Map[i][j] == 0) {
+				while (Map[i - counter][j] == 0 && i - counter > 0) {
+							counter++;
+						}
+				Map[i][j] = Map[i - counter][j];
+				Map[i - counter][j] = 0;
+				counter = 1;
+				cout << "moving down" << endl;
+			}
+		}
+	}
+}
+void Replenish(int Map[10][10]) {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (Map[i][j] == 0) {
 				Map[i][j] = rand() % 4 + 1;
 			}
 		}
 	}
 }
-/////////////////////////////////////////////////////////////////////////
--- BeJeweled/BeJeweled/Copy.cpp
+++ BeJeweled/BeJeweled/Copy.cpp
@@ -1,169 +1,292 @@
-#include <SFML/Graphics.hpp>
-#include <time.h>
-using namespace sf;
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include<ctime>
+#include<iostream>
+#include<allegro5\allegro_image.h>
+#include<allegro5\allegro_primitives.h>
+using namespace std;
 
-int ts = 54; //tile size
-Vector2i offset(48, 24);
+const float FPS = 60;
+const int SCREEN_W = 800;
+const int SCREEN_H = 800;
+const int BOUNCER_SIZE = 32;
 
-struct piece {
-	int x, y, col, row, kind, match, alpha;
-	piece() { match = 0; alpha = 255; }
-}
-grid[10][10];
-
-void swap(piece p1, piece p2)
-{
-	std::swap(p1.col, p2.col);
-	std::swap(p1.row, p2.row);
-
-	grid[p1.row][p1.col] = p1;
-	grid[p2.row][p2.col] = p2;
-}
-
+bool CheckForMatches(int matrix[20][20]);
+void MoveDown(int matrix[20][20]);
+void Replenish(int matrix[20][20]);
 
 int main()
 {
-	srand(time(0));
+	srand(time(NULL));
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *bouncer = NULL;
+	float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	bool redraw = true;
 
-	RenderWindow app(VideoMode(740, 480), "Match-3 Game!");
-	app.setFramerateLimit(60);
+	//these variables handle the mouse selection of grid slots
+	int highlightX = 0;
+	int highlightY = 0;
+	bool highlight = false;
+	int leftovers = 0;
+	bool snapped = false;
 
-	Texture t1, t2;
-	t1.loadFromFile("images/background.png");
-	t2.loadFromFile("images/gems.png");
+	int highlight2X = 0;
+	int highlight2Y = 0;
+	bool highlight2 = false;
+	int leftovers2 = 0;
+	bool snapped2 = false;
 
-	Sprite background(t1), gems(t2);
+	int temp;
 
-	for (int i = 1; i <= 8; i++)
-		for (int j = 1; j <= 8; j++)
-		{
-			grid[i][j].kind = rand() % 3;
-			grid[i][j].col = j;
-			grid[i][j].row = i;
-			grid[i][j].x = j * ts;
-			grid[i][j].y = i * ts;
-		}
+	int board[20][20];
+	for (int i = 0; i < 20; i++)
+		for (int j = 0; j < 20; j++)
+			board[i][j] = rand() % 4 + 1; //populate each slot with a #1-4
 
-	int x0, y0, x, y; int click = 0; Vector2i pos;
-	bool isSwap = false, isMoving = false;
-
-	while (app.isOpen())
-	{
-		Event e;
-		while (app.pollEvent(e))
-		{
-			if (e.type == Event::Closed)
-				app.close();
-
-			if (e.type == Event::MouseButtonPressed)
-				if (e.key.code == Mouse::Left)
-				{
-					if (!isSwap && !isMoving) click++;
-					pos = Mouse::getPosition(app) - offset;
-				}
-		}
-
-		// mouse click
-		if (click == 1)
-		{
-			x0 = pos.x / ts + 1;
-			y0 = pos.y / ts + 1;
-		}
-		if (click == 2)
-		{
-			x = pos.x / ts + 1;
-			y = pos.y / ts + 1;
-			if (abs(x - x0) + abs(y - y0) == 1)
-			{
-				swap(grid[y0][x0], grid[y][x]); isSwap = 1; click = 0;
-			}
-			else click = 1;
-		}
-
-		//Match finding
-		for (int i = 1; i <= 8; i++)
-			for (int j = 1; j <= 8; j++)
-			{
-				if (grid[i][j].kind == grid[i + 1][j].kind)
-					if (grid[i][j].kind == grid[i - 1][j].kind)
-						for (int n = -1; n <= 1; n++) grid[i + n][j].match++;
-
-				if (grid[i][j].kind == grid[i][j + 1].kind)
-					if (grid[i][j].kind == grid[i][j - 1].kind)
-						for (int n = -1; n <= 1; n++) grid[i][j + n].match++;
-			}
-
-		//Moving animation
-		isMoving = false;
-		for (int i = 1; i <= 8; i++)
-			for (int j = 1; j <= 8; j++)
-			{
-				piece &p = grid[i][j];
-				int dx, dy;
-				for (int n = 0; n<4; n++)   // 4 - speed
-				{
-					dx = p.x - p.col*ts;
-					dy = p.y - p.row*ts;
-					if (dx) p.x -= dx / abs(dx);
-					if (dy) p.y -= dy / abs(dy);
-				}
-				if (dx || dy) isMoving = 1;
-			}
-
-		//Deleting amimation
-		if (!isMoving)
-			for (int i = 1; i <= 8; i++)
-				for (int j = 1; j <= 8; j++)
-					if (grid[i][j].match) if (grid[i][j].alpha>10) { grid[i][j].alpha -= 10; isMoving = true; }
-
-		//Get score
-		int score = 0;
-		for (int i = 1; i <= 8; i++)
-			for (int j = 1; j <= 8; j++)
-				score += grid[i][j].match;
-
-		//Second swap if no match
-		if (isSwap && !isMoving)
-		{
-			if (!score) swap(grid[y0][x0], grid[y][x]); isSwap = 0;
-		}
-
-		//Update grid
-		if (!isMoving)
-		{
-			for (int i = 8; i>0; i--)
-				for (int j = 1; j <= 8; j++)
-					if (grid[i][j].match)
-						for (int n = i; n>0; n--)
-							if (!grid[n][j].match) { swap(grid[n][j], grid[i][j]); break; };
-
-			for (int j = 1; j <= 8; j++)
-				for (int i = 8, n = 0; i>0; i--)
-					if (grid[i][j].match)
-					{
-						grid[i][j].kind = rand() % 7;
-						grid[i][j].y = -ts * n++;
-						grid[i][j].match = 0;
-						grid[i][j].alpha = 255;
-					}
-		}
-
-
-		//////draw///////
-		app.draw(background);
-
-		for (int i = 1; i <= 8; i++)
-			for (int j = 1; j <= 8; j++)
-			{
-				piece p = grid[i][j];
-				gems.setTextureRect(IntRect(p.kind * 49, 0, 49, 49));
-				gems.setColor(Color(255, 255, 255, p.alpha));
-				gems.setPosition(p.x, p.y);
-				gems.move(offset.x - ts, offset.y - ts);
-				app.draw(gems);
-			}
-
-		app.display();
+										  //check for and erase matches, move spots down, replenish
+										  //repeat until no more matches found
+	while (CheckForMatches(board)) {
+		MoveDown(board);
+		Replenish(board);
 	}
+
+	//print board for testing
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++)
+			cout << board[i][j];
+		cout << endl;
+	}
+
+
+
+	al_init();
+	al_init_image_addon();
+	al_install_mouse();
+	al_init_primitives_addon();
+
+	timer = al_create_timer(1.0 / FPS);
+
+	display = al_create_display(SCREEN_W, SCREEN_H);
+
+	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+
+	al_set_target_bitmap(bouncer);
+
+	al_clear_to_color(al_map_rgb(255, 0, 255));
+
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_flip_display();
+
+	al_start_timer(timer);
+
+	while (1)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+
+			//snap the highlighted box to grid
+			if (!snapped) {
+				leftovers = highlightX % 40;
+				highlightX -= leftovers;
+				leftovers = highlightY % 40;
+				highlightY -= leftovers;
+				snapped = true;
+			}
+			if (!snapped2) {
+				leftovers2 = highlight2X % 40;
+				highlight2X -= leftovers2;
+				leftovers2 = highlight2Y % 40;
+				highlight2Y -= leftovers2;
+				snapped = true;
+			}
+			redraw = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES ||
+			ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
+
+			bouncer_x = ev.mouse.x;
+			bouncer_y = ev.mouse.y;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+
+			//if you already have a slot selected, and you're clicking next to it, select a second box
+			if (highlight == true && ev.mouse.x > highlightX - 40 && ev.mouse.x < highlightX + 80 && ev.mouse.y > highlightY - 40 && ev.mouse.y < highlightY + 80) {
+				//cout << "selected next slot!";
+				highlight2 = true;
+				highlight2X = ev.mouse.x;
+				highlight2Y = ev.mouse.y;
+				snapped2 = false;
+				//swap the two boxes 
+				temp = board[highlight2Y / 40][highlight2X / 40];
+				board[highlight2Y / 40][highlight2X / 40] = board[highlightY / 40][highlightX / 40];
+				board[highlightY / 40][highlightX / 40] = temp;
+				while (CheckForMatches(board)) {
+					MoveDown(board);
+					Replenish(board);//may be premature
+					highlight = false;
+					highlight2 = false;
+				}
+			}
+			//otherwise, select a new box
+			else
+			{
+				highlight = true;
+				highlight2 = false;
+				highlightX = ev.mouse.x;
+				highlightY = ev.mouse.y;
+				snapped = false;
+			}
+
+		}
+		//render section
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+
+
+
+
+
+			for (int i = 0; i < 20; i++)
+				for (int j = 0; j < 20; j++) {
+					al_draw_rectangle(i * 40, j * 40, i * 40 + 40, j * 40 + 40, al_map_rgb(80, 80, 80), 3);
+					if (board[j][i] == 1)
+						al_draw_filled_circle(i * 40 + 20, j * 40 + 20, 15, al_map_rgb(50, 150, 50));
+					if (board[j][i] == 2)
+						al_draw_filled_rectangle(i * 40 + 5, j * 40 + 5, i * 40 + 35, j * 40 + 35, al_map_rgb(50, 50, 250));
+					if (board[j][i] == 3)
+						al_draw_filled_triangle(i * 40 + 20, j * 40 + 2, i * 40 + 2, j * 40 + 38, i * 40 + 38, j * 40 + 38, al_map_rgb(150, 50, 250));
+					if (board[j][i] == 4) {
+						al_draw_filled_triangle(i * 40 + 20, j * 40 + 2, i * 40 + 2, j * 40 + 38, i * 40 + 38, j * 40 + 38, al_map_rgb(150, 250, 50));
+						al_draw_filled_triangle(i * 40 + 20, j * 40 + 38, i * 40 + 2, j * 40 + 2, i * 40 + 38, j * 40 + 2, al_map_rgb(150, 250, 50));
+					}
+				}
+
+			if (highlight) {
+				al_draw_rectangle(highlightX, highlightY, highlightX + 40, highlightY + 40, al_map_rgb(255, 255, 255), 5);
+			}
+			if (highlight2) {
+				al_draw_rectangle(highlight2X, highlight2Y, highlight2X + 40, highlight2Y + 40, al_map_rgb(0, 255, 255), 5);
+			}
+			al_flip_display();
+
+			//cout << "checking for matches..." << endl;
+			//system("pause");
+			//CheckForMatches(board);
+			//al_flip_display();
+			//system("pause");
+
+
+			/*
+
+			cout << "moving columns down" << endl;
+			system("pause");
+			MoveDown(board);
+			al_flip_display();
+
+			cout << "replenishing board" << endl;
+			system("pause");
+			Replenish(board);
+			al_flip_display();
+			*/
+
+
+		}
+	}
+
+	al_destroy_bitmap(bouncer);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+
 	return 0;
+}
+
+//checks for matches of 3
+//then, goes through and changes everything in that row/column that are 3+ matches into 0s
+bool CheckForMatches(int matrix[20][20]) {
+	int count = 1;
+	bool foundMatch = false;
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			if ((matrix[i][j] == matrix[i][j + 1]) && (matrix[i][j] == matrix[i][j + 2])) {
+				//cout << "horizontal match found at " << i << " , " << j << endl;
+				foundMatch = true;
+				while (matrix[i][j] == matrix[i][j + count]) {
+					matrix[i][j + count] = 0;
+					count++;
+				}
+				matrix[i][j] = 0;
+				count = 1;
+			}
+		}
+	}
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			if ((matrix[i][j] == matrix[i + 1][j]) && (matrix[i][j] == matrix[i + 2][j])) {
+				//cout << "vertical match found at " << i << " , " << j << endl;
+				foundMatch = true;
+				while (matrix[i][j] == matrix[i + count][j]) {
+					matrix[i + count][j] = 0;
+					count++;
+				}
+				matrix[i][j] = 0;
+				count = 1;
+			}
+		}
+	}
+	return foundMatch;
+}
+
+void MoveDown(int matrix[20][20]) {
+	int counter = 1;
+	for (int i = 19; i > 0; i--) {
+		for (int j = 0; j < 20; j++) {
+			if (matrix[i][j] == 0) {
+				//counts how many spaces we're going to move the 0 down
+				while (matrix[i - counter][j] == 0 && i - counter > 0) {
+					counter++;
+				}
+				//move the slot above my current slot down
+				matrix[i][j] = matrix[i - counter][j];
+				//moves the 0 that used to be there up
+				matrix[i - counter][j] = 0;
+				counter = 1;
+				//cout << "moving down" << endl;
+			}
+		}
+	}
+}
+
+void Replenish(int matrix[20][20]) {
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			if (matrix[i][j] == 0) {
+				matrix[i][j] = rand() % 4 + 1;
+			}
+		}
+	}
 }
